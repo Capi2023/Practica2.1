22f088e47b648e17ae1e3f18fce565b4d103a542
Emiliano Garcia Cordero <124217973+Capi2023@users.noreply.github.com>
2023-10-11 14:22:09 -0700

    Update hola.py

--- hola.py
+++ hola.py
@@ -1,3 +1,12 @@
+## Depto de Sistemas y Computación
+## Ing. En Sistemas Computacionales
+## SISTEMAS PROGRAMABLES 23a
+## Autor (es): Emiliano García Cordero
+## Repositorio: https://github.com/Capi2023/Practica2.1
+## Fecha de revisión:   11/10/2023
+## Objetivo: 2.1.1 Practica De inicio es la básico de Desplegar algo en pantalla, algunos quieren el logo de ISC, esta bien, otro texto simple, se agradece,  en el OLED DIsplay
+##   
+
 from machine import Pin, I2C
 from ssd1306 import SSD1306_I2C
 
